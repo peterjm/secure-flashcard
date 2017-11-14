2b493d627154ef7bed1924c157203f09b2b59fb0
Peter McCracken <peter.mccracken@shopify.com>
2017-11-14 16:20:34 -0500

    force all traffic to be SSL

---
+++ config/initializers/force_ssl.rb
@@ -0,0 +1,1 @@
+Rails.application.config.force_ssl = true
--- config/puma.rb
+++ config/puma.rb
@@ -52,5 +52,14 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # end
 #
 
+# On development, run ssl server on port 3001
+if ENV.fetch("RAILS_ENV") == 'development'
+  ssl_bind '127.0.0.1', '3001', {
+    key: ENV.fetch("SSL_KEY_PATH"),
+    cert: ENV.fetch("SSL_CERT_PATH"),
+    verify_mode: 'none'
+  }
+end
+
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
