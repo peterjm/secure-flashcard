25cd9e03f10dceb7f4245803a6b3492ae43fc32c
Peter McCracken <peter.mccracken@shopify.com>
2017-11-21 10:35:38 -0500

    ssl only in production

--- config/puma.rb
+++ config/puma.rb
@@ -52,8 +52,8 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # end
 #
 
-if ENV.fetch("RAILS_ENV") == 'development' || ENV.fetch("RAILS_ENV") == 'production'
-  ssl_bind '127.0.0.1', '3001', {
+if ENV.fetch("RAILS_ENV") == 'production'
+  ssl_bind '127.0.0.1', ENV.fetch('PORT'), {
     key: ENV.fetch("SSL_KEY_PATH"),
     cert: ENV.fetch("SSL_CERT_PATH"),
     verify_mode: 'none'
