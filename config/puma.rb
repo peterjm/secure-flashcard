96ce643213493f25bdc81cce5f527316fa39bb7b
Peter McCracken <peter.mccracken@shopify.com>
2018-04-25 15:45:04 -0400

    remove production config stuff

--- config/puma.rb
+++ config/puma.rb
@@ -30,13 +30,5 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 #
 # preload_app!
 
-if ENV.fetch("RAILS_ENV") == 'production'
-  ssl_bind '127.0.0.1', ENV.fetch('PORT'), {
-    key: ENV.fetch("SSL_KEY_PATH"),
-    cert: ENV.fetch("SSL_CERT_PATH"),
-    verify_mode: 'none'
-  }
-end
-
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
