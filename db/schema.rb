cddcdfb23eb74a4cfc07399fd5135bc2945020cb
Peter McCracken <peter.mccracken@shopify.com>
2021-11-07 09:09:21 -0500

    rearrange migration timestamps for active storage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_07_133525) do
+ActiveRecord::Schema.define(version: 2021_11_07_130229) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
