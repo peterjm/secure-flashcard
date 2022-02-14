d4cbdeb95f9d65393de2d4c539f926dce30f8172
Peter McCracken <peter@petermccracken.com>
2022-02-13 21:29:21 -0500

    set assets.compile = false

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,7 @@ Rails.application.config.assets.version = "1.0"
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+# This setting is recommended by Heroku
+# https://devcenter.heroku.com/articles/rails-asset-pipeline#compile-set-to-true-in-production
+Rails.application.config.assets.compile = false
