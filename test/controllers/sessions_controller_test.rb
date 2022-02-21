ebc063fa3cf624b1f1cd53bedf495edb7ea36fa7
Peter McCracken <peter@petermccracken.com>
2022-02-21 13:25:58 -0500

    add SessionsController tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -3,7 +3,54 @@
 require "test_helper"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#new renders successfully when logged out" do
+    get login_path
+    assert_response :success
+  end
+
+  test "#new redirects when logged in" do
+    log_in
+    get login_path
+    assert_redirected_to root_path
+  end
+
+  test "#create logs in when given successful auth params" do
+    log_in
+    assert_redirected_to root_path
+  end
+
+  test "#create redirects with error code when login not successful" do
+    log_in(mock_auth: successful_google_auth_hash(email: "unexpected.email@example.com"))
+    assert_response :unprocessable_entity
+  end
+
+  test "#create redirects when logged in" do
+    log_in
+    log_in
+    assert_redirected_to root_path
+  end
+
+  test "#error renders with error code when logged out" do
+    log_in(mock_auth: :invalid_credentials)
+    follow_redirect! # follows the redirect from the callback phase to the error page
+    assert_response :unprocessable_entity
+  end
+
+  test "#error redirects when logged in" do
+    log_in
+    log_in(mock_auth: :invalid_credentials)
+    follow_redirect! # follows the redirect from the callback phase to the error page
+    assert_redirected_to root_path
+  end
+
+  test "#destroy logs out when logged in" do
+    log_in
+    delete logout_path
+    assert_redirected_to login_path
+  end
+
+  test "#destroy does nothing when logged out" do
+    delete logout_path
+    assert_redirected_to login_path
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,21 +5,29 @@ require "rails/test_help"
 
 OmniAuth.config.test_mode = true
 
-OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new({
-  info: {
-    email: Rails.application.credentials.google.allowed_account,
-  }
-})
-
 module ActiveSupport
   class TestCase
-    def log_in
+    setup do
+      OmniAuth.config.mock_auth[:google] = nil
+    end
+
+    def inspect
+      # overriding this so it doesn't print hundreds of lines of output on NoMethodError
+      self.class.name
+    end
+
+    def log_in(mock_auth: successful_google_auth_hash)
+      OmniAuth.config.mock_auth[:google] = mock_auth
       post "/auth/google"
       follow_redirect!
     end
 
     def log_out
       delete logout_path
+    end
+
+    def successful_google_auth_hash(email: Rails.application.credentials.google.allowed_account)
+      OmniAuth::AuthHash.new({ info: { email: email } })
     end
   end
 end
