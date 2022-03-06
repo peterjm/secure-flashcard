ddad1e0de7bdffb1089af4e06f8e481c6942899b
Peter McCracken <peter@petermccracken.com>
2022-03-06 13:31:25 -0500

    fix javascript

--- app/javascript/side_menu.js
+++ app/javascript/side_menu.js
@@ -1,7 +1,5 @@
-(function (window, document) {
-  document.addEventListener("turbo:load", function() {
-    document.getElementById('nav-toggle').onclick = function() {
-      document.getElementById("nav-content").classList.toggle("hidden");
-    };
-  });
-}(this, this.document));
+document.addEventListener("turbo:load", function() {
+  document.getElementById('nav-toggle').onclick = function() {
+    document.getElementById("nav-content").classList.toggle("hidden");
+  };
+});
