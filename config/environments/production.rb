f144750ba813dc6d76fffe8b77d7ad2e9e837047
Peter McCracken <peter@petermccracken.com>
2023-11-06 14:26:02 -0500

    fix style

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   config.active_support.report_deprecations = false
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
+  config.log_formatter = Logger::Formatter.new
 
   # Use a different logger for distributed setups.
   # require "syslog/logger"
@@ -88,5 +88,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # sqlite is fine for this app
-  config.active_record.sqlite3_production_warning=false
+  config.active_record.sqlite3_production_warning = false
 end
