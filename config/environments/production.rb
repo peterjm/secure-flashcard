47c4667909b1e342ddefe4e1f114faa6bd85a447
Peter McCracken <peter@petermccracken.com>
2026-02-13 23:13:11 -0500

    remove sqlite warning for production, again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,4 +86,7 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # sqlite is fine for this app
+  config.active_record.sqlite3_production_warning = false
 end
